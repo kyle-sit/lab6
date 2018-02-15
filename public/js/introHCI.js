fa8b43e547692f46a86b047e469fa675630598b9
Kyle Sit <ktsit@ucsd.edu>
2018-02-14 20:06:23 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,21 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/' + idNumber, callBack);
+}
+
+function randomizeColors() {
+
+}
+
+function callBack(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['summary'] +
+    '</small></p></a>'; 
+
+	$('.project .details').html(projectHTML);
 }
